35a01aa4d14f07b54aa6555cde0e38fbac59e857
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-05-15 18:03:23 -0300

    feat: entities folders

---
+++ Models/Dado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avaliacao2703.Models
+{
+    public class Dado
+    {
+        public int NumeroDeLado { get; set; }
+        public int NumeroSorteado { get; set; }
+
+        public string Rolar(int max)
+        {
+            Random rand = new Random();
+            if (max  < 0) { return ""; }
+
+            int min = 1;
+
+            int v = rand.Next(min, max + 1);
+            return v.ToString();
+        }
+    }
+}
