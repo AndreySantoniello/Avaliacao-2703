2367adbf2177f52369a6ee6790a90b7dc3f288dc
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-05-15 18:05:54 -0300

    refactor: remove trash code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,29 +2,9 @@
 {
     public partial class MainPage : ContentPage
     {
-        public int index;
-
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void BNTRolar_Clicked(object sender, EventArgs e)
-        {
-            Random rand = new Random();
-
-            if (index !=  -1 && pickerDado.SelectedItem != null)
-            {
-                int min = 1;
-                int max = (int)pickerDado.SelectedItem;
-
-                int v = rand.Next(min, max + 1);
-                dadoValue.Text = v.ToString();
-            }else
-            {
-                DisplayAlert("Atenção", "Você precisa selecionar um dado primeiro.", "OK");
-            }
-        }
     }
-
 }
