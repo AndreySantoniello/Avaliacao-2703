f0448d9c34018d87f440384db3d2b7ef14ef39d7
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-05-15 18:20:34 -0300

    feat: observable object mvvm pattern

--- ViewModels/DadoViewModel.cs
+++ ViewModels/DadoViewModel.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace avaliacao2703.ViewModels
 {
     internal class DadoViewModel
     {
+        [ObservableProperty]
+        private int quantidadeDeLados;
+
+        [ObservableProperty]
+        private int numeroSorteado;
     }
 }
